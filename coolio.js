32cb734e0485b3b32372b2a85590255004713ac9
Nico <ndiaz413@west-mec.org>
2024-02-28 18:22:53 -0700

    Created Variables
    Began creating variables for each element for the website.

---
+++ coolio.js
@@ -0,0 +1,30 @@
+const body = document.getElementsByTagName('body')
+
+// Create header tag + insides
+const header = document.createElement('header')
+const nav = document.createElement('nav')
+const ul = document.createElement('ul')
+const navHome = document.createElement('li')
+const navGallery = document.createElement('li')
+const navServices = document.createElement('li')
+const navOrder = document.createElement('li')
+const divSearch = document.createElement('div')
+
+// Logo
+const h1Main = document.createElement('h1')
+const h2Bottom = document.createElement('h2')
+
+// Gallerys
+const sectionVideo = document.createElement('section')
+const sectionImage = document.createElement('section')
+const divVideo = document.createElement('div')
+const divImageAstro = document.createElement('div')
+const divImageChute = document.createElement('div')
+const divImageLanding = document.createElement('div')
+const divImageRocket = document.createElement('div')
+const divImageLaunch = document.createElement('div')
+const divImageDock = document.createElement('div')
+
+// Services
+const sectionServices = document.createElement('section')
+header.body.appendChild(header)
